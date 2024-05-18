8c946a3c98c9862b307603717c5e3ae99d2afaa5
Mbaruku2003 <leonardjembe21@gmail.com>
2024-05-18 06:15:21 -0700

    trial

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,8 @@ import uuid
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
+        """Initialise init."""
+
         if kwargs:
             for key, value in kwargs.items():
                 if key == '__class__':
@@ -21,15 +23,15 @@ class BaseModel:
     def __str__(self):
         """ should print: [<class name>] (<self.id>) <self.__dict__>"""
 
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Updates the public instance attribute updated_at with the current datetime."""
+        """Updates the public instance attribute updated_at."""
 
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of __dict__ of the instance."""
+        """Returns a dictionary containing all keys/values of __dict__."""
 
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
